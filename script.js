40a39acfd5ff9a4f6750cf707f0c2fcb2c68ccb9
Christopher Castaneda <40323128+Christopherx26@users.noreply.github.com>
2019-08-28 10:51:29 -0500

    Add files via upload
    temp redesign

---
+++ script.js
@@ -0,0 +1,24 @@
+// Modal Image Gallery
+function onClick(element) {
+  document.getElementById("img01").src = element.src;
+  document.getElementById("modal01").style.display = "block";
+  var captionText = document.getElementById("caption");
+  captionText.innerHTML = element.alt;
+}
+// <!-- Add Google Maps -->
+
+function myMap()
+{
+  myCenter=new google.maps.LatLng(26.1498, 97.9136);
+  var mapOptions= {
+    center:myCenter,
+    zoom:12, scrollwheel: false, draggable: false,
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+
+  var marker = new google.maps.Marker({
+    position: myCenter,
+  });
+  marker.setMap(map);
+}
